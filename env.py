ed783173f6fa8fcd3e124bb62305ff8af7156e4d
onslaught-2342 <168327454+onslaught-2342@users.noreply.github.com>
2024-07-30 10:58:33 +0500

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 DATABASE_FILE = 'file_data.db'
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1223502869337083914/Q3Zs-vonA6gC58KsK5w6Vspc4565Q1WbDO1UxC14gbZgVtQnwDn88D4WdvBnCRHnxtqo'
+WEBHOOK_URL = 'plz enter your url here for discord webhook'
